be9f0729a1cbc56c17c935c2b6d8fc489eaa06ab
Christoph Seitz <christoph_seitz@gmx.de>
2011-01-12 18:27:07 +0100

    Cosmetic Changes

--- lib/volcontrol.lua
+++ lib/volcontrol.lua
@@ -1,5 +1,10 @@
 -- {{
 -- Lua Code for volumen control widget
+-- Copyright (c) 2011 Christoph Seitz
+-- Licensed under MIT License
+-- See LICENSE for more info
+-- }
+
 local io = require("io")
 local os = require("os")
 volcontrol = { }
@@ -21,47 +26,47 @@ end
 
 function volcontrol:register_events()
 	self.widget:buttons(awful.util.table.join(
-	awful.button({ }, 4, function () self:vol_up(10) end),
-	awful.button({ }, 5, function () self:vol_down(10) end),
-	awful.button({"Shift" }, 4, function () self:vol_up(5)end),
-	awful.button({"Shift" }, 5, function () self:vol_down(5) end),
-	awful.button({ }, 1, function () self:mute_toggle() end)
+		awful.button({ }, 4, function () self:vol_up(10) end),
+		awful.button({ }, 5, function () self:vol_down(10) end),
+		awful.button({"Shift" }, 4, function () self:vol_up(5)end),
+		awful.button({"Shift" }, 5, function () self:vol_down(5) end),
+		awful.button({ }, 1, function () self:mute_toggle() end)
 	))
 end
 
 function volcontrol:mute_toggle() 
-if self.mute == true then
-self.mute=false
-os.execute("amixer -q sset "..self.mixer.." ".."on")
-self:update_ui()
-return
-end
-if self.mute == false then
-self.mute=true
-os.execute("amixer -q sset "..self.mixer.." ".."off")
-self:update_ui()
-return
-end
+	if self.mute == true then
+		self.mute=false
+		os.execute("amixer -q sset "..self.mixer.." ".."on")
+		self:update_ui()
+		return
+	end
+	if self.mute == false then
+		self.mute=true
+		os.execute("amixer -q sset "..self.mixer.." ".."off")
+		self:update_ui()
+		return
+	end
 end
 
 function volcontrol:vol_up(x) 
-self.vol = self.vol + x
-if self.vol >100 then
-self.vol = 100
-end
-os.execute("amixer -q sset "..self.mixer.." "..tostring(self.vol).."%")
-self:update_vol()
-self:update_ui()
+	self.vol = self.vol + x
+	if self.vol >100 then
+		self.vol = 100
+	end
+	os.execute("amixer -q sset "..self.mixer.." "..tostring(self.vol).."%")
+	self:update_vol()
+	self:update_ui()
 end	
 
 function volcontrol:vol_down(x) 
-self.vol = self.vol - x
-if self.vol <0 then
-self.vol = 0
-end
-os.execute("amixer -q sset "..self.mixer.." "..tostring(self.vol).."%")
-self:update_vol()
-self:update_ui()
+	self.vol = self.vol - x
+	if self.vol <0 then
+		self.vol = 0
+	end
+	os.execute("amixer -q sset "..self.mixer.." "..tostring(self.vol).."%")
+	self:update_vol()
+	self:update_ui()
 end
 
 function volcontrol:run() 
@@ -71,22 +76,22 @@ function volcontrol:run()
 end
 
 function split(str, pat)
-   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-    local fpat = "(.-)" .. pat
-      local last_end = 1
-   local s, e, cap = str:find(fpat, 1)
-     while s do
-          if s ~= 1 or cap ~= "" then
-   	 table.insert(t,cap)
-      end
-           last_end = e+1
-          s, e, cap = str:find(fpat, last_end)
-      end
+	local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+	local fpat = "(.-)" .. pat
+      	local last_end = 1
+   	local s, e, cap = str:find(fpat, 1)
+     	while s do
+        	if s ~= 1 or cap ~= "" then
+   	 		table.insert(t,cap)
+      		end
+           	last_end = e+1
+          	s, e, cap = str:find(fpat, last_end)
+      	end
         if last_end <= #str then
-       cap = str:sub(last_end)
-           table.insert(t, cap)
-     end
-return t
+       		cap = str:sub(last_end)
+           	table.insert(t, cap)
+     	end
+	return t
 end
 
 function volcontrol:update_vol()
@@ -108,7 +113,6 @@ function volcontrol:update_vol()
 end
 
 function volcontrol:update_ui() 
-	print(tostring(self.vol))
 	if self.vol <= 30 then
 		self.widget.image = image(awful.util.getdir("config") .. "/lib/icons/audio-volume-low.png")
 	end
